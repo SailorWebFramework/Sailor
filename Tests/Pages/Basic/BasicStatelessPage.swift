7e24aec596692e92be7262cca58c58980201306c
Joshua Davis <chess.ofx@gmail.com>
2023-12-29 04:32:57 -0500

    attempted to create testing dir, some bug-fixes, started docker

---
+++ Tests/Pages/Basic/BasicStatelessPage.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by Joshua Davis on 12/28/23.
+//
+
+import Foundation
+//import Sailor
+//
+//struct BasicStatelessPage: Page {
+//    var hi: some Page {
+//        Div {
+//            Button("hello world")
+//        }
+//    }
+//    var body: some Page {
+//       Div("")
+//    }
+//}
+
---
+++ Tests/SailorLibraryTests/BasicTests.swift
@@ -0,0 +1,39 @@
+//
+//  File.swift
+//  
+//
+//  Created by Joshua Davis on 12/28/23.
+//
+
+import XCTest
+//@testable import Sailor
+
+final class BasicTests: XCTestCase {
+   
+    func testBasicStateMemoryLeak() throws {
+        
+    }
+    
+    func testStatefulPage() throws {
+//        guard let documentBody = SailorGlobal.documentBody else {
+//            XCTAssert(true, "document body reference does not exist")
+//            return
+//        }
+//        
+//        var hp: CustomNode? = CustomNode(page: BasicStatefulPage(), aboveElement: documentBody)
+//
+//        hp!.build(child: hp!.page.body)
+////        hp!.printNode()
+//        
+//        XCTAssertEqual(SailorGlobal.states.count, 1, "State not initialized correctly")
+//
+//        hp = nil
+//
+//        XCTAssertEqual(SailorGlobal.states.count, 0, "Memory not Deinitialized")
+//        
+    }
+    
+    func testLoginFailure() {
+        
+    }
+}
