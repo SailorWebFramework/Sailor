3ba6e29dc9b7f544b865109596c4d9b84379a39e
Joshua Davis <chess.ofx@gmail.com>
2023-12-29 04:32:57 -0500

    attempted to create testing dir, some bug-fixes, started docker

--- Playground/TestWebsite.swift
+++ Playground/TestWebsite.swift
@@ -8,3 +8,5 @@ struct TestWebsite: App {
         HomePage()
     }
 }
+
+
---
+++ Tests/Pages/Basic/BasicStatefulPage.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  
+//
+//  Created by Joshua Davis on 12/28/23.
+//
+
+import Foundation
+//import Sailor
+//
+//struct BasicStatefulPage: Page {
+//    @State var hello: Int = 0
+//    var hi: some Page {
+//        Div {
+//            Button("hello: \(hello)")
+//                .onClick {
+//                    hello += 1
+//                }
+//        }
+//    }
+//    var body: some Page {
+//       Div("")
+//    }
+//}
+
