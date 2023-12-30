bf48fe03d1fec626958e8d23b1f057410ac76209
Joshua Davis <chess.ofx@gmail.com>
2023-12-29 22:51:28 -0500

    broke everything again, split to sailboat, pre removing JSKit from nodes

---
+++ Sailboat/Sources/Page/Operator/Operators/List.swift
@@ -0,0 +1,30 @@
+//
+//  List.swift
+//  
+//
+//  Created by Joshua Davis on 10/12/23.
+//
+
+import Foundation
+
+public struct List: Operator {
+    
+    public var id: UUID = UUID()
+
+    public var children: [any Page]
+
+    public var body: some Page {
+        InternalError.recursingInPageBody(name: "container")
+        return self
+    }
+    
+    public init() {
+        self.init([])
+    }
+
+    public init(_ children: [any Page]) {
+        self.children = children
+
+    }
+
+}
---
+++ Sailboat/Sources/State/State.swift
@@ -0,0 +1,61 @@
+//
+//  State.swift
+//  
+//
+//  Created by Joshua Davis on 10/8/23.
+//
+
+import Foundation
+
+public typealias StateValue = any Equatable
+public typealias StateNode = LinkedListNode<StateValue>
+
+@propertyWrapper
+public class State<Value: Equatable> {
+    
+    private var node: StateNode
+
+    public var wrappedValue: Value {
+        get {
+            return getValue()
+        }
+        set {
+            setValue(newValue)
+        }
+    }
+    
+    public var projectedValue: Binding<Value> {
+        Binding(
+            get: { self.getValue() },
+            set: { self.setValue($0) }
+        )
+    }
+    
+    public init(wrappedValue: Value) {
+        self.node = SailboatGlobal.shared.states.append(wrappedValue)
+    }
+    
+    // TODO: fix state not deiniting properly look at example, happens when an initially built view leaves the DOM
+    deinit {
+        print("Deinitializing State: \(self.node)")
+        self.node.remove()
+    }
+    
+    private func getValue() -> Value {
+        // TODO: UNSAFE, but probably fine
+        self.node.value as! Value
+    }
+    
+    private func setValue(_ value: Value) {
+        // TODO: check if no change conform to equatable?
+        
+        if self.node.value as? Value == value {
+            return
+        }
+        
+        // TODO: Maybe batch updates eventually
+        SailboatGlobal.shared.update(state: self.node, newValue: value)
+        
+    }
+
+}
