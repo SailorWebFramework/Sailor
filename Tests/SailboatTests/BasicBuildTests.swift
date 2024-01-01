a748f0794ab53e3f1cef7e27c61b80b1e52c7f7e
Joshua Davis <chess.ofx@gmail.com>
2024-01-01 02:29:05 -0500

    tests build, new JSNOde (not working), also non-functional reconcile function

--- Tests/SailboatTests/BasicBuildTests.swift
+++ Tests/SailboatTests/BasicBuildTests.swift
@@ -10,56 +10,56 @@ import XCTest
 
 final class BasicBuildTests: XCTestCase {
     
-    var sailboatManager: (any GlobalDOMManager)! {
+    var sailboatManager: (any AppManager)! {
         SailboatGlobal.shared
     }
     
     override class func setUp() {
         SailboatGlobal.initialize(DefaultManager())
     }
+//
+//    func testInitialBasicStateMemoryLeak() throws {
+//        sailboatManager.build(page: BasicStatelessPage())
+//
+////        XCTAssertEqual(State.count, 0, "State not initialized correctly")
+////
+////        sailboatManager.build(page: BasicStatefulPage0())
+////
+////        XCTAssertEqual(State.count, 1, "State not initialized correctly")
+////
+////        sailboatManager.build(page: BasicStatelessPage())
+////
+////        XCTAssertEqual(State.count, 0, "State not removed correctly")
+////
+////        sailboatManager.build(page: BasicStatefulPage1())
+////
+////        XCTAssertEqual(State.count, 2, "State not initialized correctly")
+////
+////        sailboatManager.build(page: BasicStatefulPage1())
+////
+////        XCTAssertEqual(State.count, 2, "State not initialized correctly")
+////
+////        sailboatManager.build(page: BasicStatefulPage0())
+////
+////        XCTAssertEqual(State.count, 1, "State not initialized correctly")
+//
+//        return
+//    }
     
-    func testInitialBasicStateMemoryLeak() throws {
-        sailboatManager.build(page: BasicStatelessPage())
-
-        XCTAssertEqual(sailboatManager.states.count, 0, "State not initialized correctly")
-
-        sailboatManager.build(page: BasicStatefulPage0())
-        
-        XCTAssertEqual(sailboatManager.states.count, 1, "State not initialized correctly")
-        
-        sailboatManager.build(page: BasicStatelessPage())
-
-        XCTAssertEqual(sailboatManager.states.count, 0, "State not removed correctly")
-
-        sailboatManager.build(page: BasicStatefulPage1())
-        
-        XCTAssertEqual(sailboatManager.states.count, 2, "State not initialized correctly")
-
-        sailboatManager.build(page: BasicStatefulPage1())
-        
-        XCTAssertEqual(sailboatManager.states.count, 2, "State not initialized correctly")
-
-        sailboatManager.build(page: BasicStatefulPage0())
-        
-        XCTAssertEqual(sailboatManager.states.count, 1, "State not initialized correctly")
-
-        return
-    }
-    
-    func testStatefulPageBuild() throws {
-        
-        sailboatManager.build(page: BasicStatefulPage2())
-
-        XCTAssertEqual(sailboatManager.states.count, 3, "State not initialized correctly")
-
-        XCTAssertEqual(sailboatManager.states[0] as! Int, 2, "State not initialized correctly")
-        XCTAssertEqual(sailboatManager.states[1] as! Int, 1, "State not initialized correctly")
-        XCTAssertEqual(sailboatManager.states[2] as! Int, 0, "State not initialized correctly")
-        
-        // verify build tree
-        sailboatManager.body?.printNode()
-    }
-    
+//    func testStatefulPageBuild() throws {
+//        
+//        sailboatManager.build(page: BasicStatefulPage2())
+//
+////        XCTAssertEqual(State.count, 3, "State not initialized correctly")
+//
+////        XCTAssertEqual(sailboatManager.states[0] as! Int, 2, "State not initialized correctly")
+////        XCTAssertEqual(sailboatManager.states[1] as! Int, 1, "State not initialized correctly")
+////        XCTAssertEqual(sailboatManager.states[2] as! Int, 0, "State not initialized correctly")
+//        
+//        // verify build tree
+//        sailboatManager.body?.printNode()
+//    }
+//    
     func testLoginFailure() {
         
     }
