1edc279e79be0434aa8f8b4f7530e7c26d5c9b40
Joshua Davis <chess.ofx@gmail.com>
2024-02-27 17:32:38 -0500

    some environment changes

---
+++ Sailor/Sources/DOM/JSNode/JSNodeRenderer.swift
@@ -0,0 +1,49 @@
+//
+//  File.swift
+//  
+//
+//  Created by Joshua Davis on 2/27/24.
+//
+
+import Sailboat
+
+final class JSNodeRenderer: Renderable {
+    func render() {
+        
+    }
+    
+    func remove() {
+        
+    }
+    
+    func replace(with renderable: Sailboat.Renderable) {
+        
+    }
+    
+    func update(attributes: [String : String]) {
+        
+    }
+    
+    func addAttribute(name: String, value: String) {
+        
+    }
+    
+    func addEvent(name: String, value: (Sailboat.EventResult) -> Void) {
+        
+    }
+    
+    //MARK - Special sailor events managed through the renderer
+    
+    func onAppear() {
+        
+    }
+    
+    func onDisappear() {
+        
+    }
+    
+    func onUpdate() {
+        
+    }
+    
+}
---
+++ Sailor/Sources/SailboatSupport/CustomPage.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by Joshua Davis on 2/27/24.
+//
+
+import Sailboat
+
+//public protocol CustomPage: Page {
+//
+//    // todo: typeIdentifier here for signals
+//    associatedtype Body: Page
+//
+//    /// body of the page element, resultBuilder can contain HTMLElements and custom Page elements
+//    var head: Body { get }
+//
+//}
+//
+//extension CustomPage {
+//    var head: some Page {
+//        List()
+//    }
+//}
