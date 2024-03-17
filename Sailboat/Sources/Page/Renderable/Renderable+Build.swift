4ea605c9f1195e219e9049eebbada49a51de134c
Joshua Davis <chess.ofx@gmail.com>
2024-03-17 02:56:26 -0700

    head mostly works correctly

--- Sailboat/Sources/Page/Renderable/Renderable+Build.swift
+++ Sailboat/Sources/Page/Renderable/Renderable+Build.swift
@@ -8,48 +8,6 @@
 public extension Renderable {
     
     ///
-    func build(_ element: any Element) {
-        build(page: element, parent: nil)
-    }
-
-    /// build a page to this renderer and add it to parent
-    private func build(page: any Page, parent: (any Element)?) {
-        // if page is an Operator
-        if let page = page as? any Operator {
-            
-            // add children
-            for child in page.children {
-                build(page: child, parent: parent)
-            }
-            
-            return
-        }
-        
-        // if page is an HTMLElement
-        if let page = page as? any Element {
-            
-            // run the page builder closure to create an operator node
-            let operatorPage = page.content()
-            
-            SailboatGlobal.managedPages.registerElement(page, operatorPage)
-            
-            build(page: operatorPage, parent: page)
-            
-            // render current page to parent
-            page.renderer.render()
-            
-            if let parent = parent {
-                page.renderer.addToParent(parent)
-            }
-            
-            return
-        }
-        
-        build(page: page.body, parent: parent)
-        
-    }
-    
-    ///
     internal func build(_ newContent: any Page, after index: Int) -> Int {
         guard let myElement = SailboatGlobal.manager.managedPages.elements[self.elementID] else {
             fatalError("element doesnt exist in global state")
@@ -59,7 +17,7 @@ public extension Renderable {
         
         if let newContent = newContent as? any Element {
             
-            newContent.renderer.build(newContent)
+            RenderableUtils.build(newContent)
 
             if newIndex != -1 {
                 newContent.renderer.insertAfter(newIndex, parent: myElement)
