c22c776dce24a81928eca3fc1b0f6b51d9aa3389
Joshua Davis <chess.ofx@gmail.com>
2024-04-11 10:59:11 -0700

    global css properites work

--- SailorCore/Sources/Custom/Attributes/Element+Style.swift
+++ SailorCore/Sources/Custom/Attributes/Element+Style.swift
@@ -9,7 +9,7 @@ import Sailboat
 
 public extension Element {
     // TODO: make a style builder
-    func style(_ completion: @escaping () -> Style) -> Self {
+    func style(@StyleBuilder _ completion: @escaping () -> Style) -> Self {
         var copy = self
         
         copy.attributes["style"] = completion
