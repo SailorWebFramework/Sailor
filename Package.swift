93192ad1445baf944bf45a79bc9586ab33cd309d
Joshua Davis <chess.ofx@gmail.com>
2024-04-30 01:55:23 -0700

    reorganized repo

--- Package.swift
+++ Package.swift
@@ -1,6 +1,5 @@
 // swift-tools-version: 5.9
 import PackageDescription
-import CompilerPluginSupport
 
 let package = Package(
     name: "Sailor",
@@ -15,10 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url:"https://github.com/swiftwasm/JavaScriptKit", from: "0.18.0"),
-        // TODO: automate this, but for now switch below comment when debugging
-        //.package(path: "../Sailboat")
         .package(url:"https://github.com/SailorWebFramework/Sailboat", from: "0.2.0"),
-
     ],
     targets: [
         .target(
@@ -28,15 +24,13 @@ let package = Package(
                 "SailorShared",
                 .target(name: "SailorWeb", condition: .when(platforms: [.wasi]))
                 // more targets here
-            ],
-            path: "SailorCore"
+            ]
         ),
         .target(
             name: "SailorShared",
             dependencies: [
                 "Sailboat"
-            ],
-            path: "SailorShared"
+            ]
         ),
         .target(
             name: "SailorWeb",
@@ -45,19 +39,7 @@ let package = Package(
                 "SailorShared",
                 .product(name: "JavaScriptKit", package: "JavaScriptKit"),
                 .product(name: "JavaScriptEventLoop", package: "JavaScriptKit")
-            ],
-            path: "SailorWeb"
+            ]
         ),
-//        .macro(
-//            name: "SailorMacros",
-//            dependencies: [
-//                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-//                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
-//                "SailorCore"
-//            ],
-//            path: "SailorMacros"
-//
-//        ),
-
     ]
 )
